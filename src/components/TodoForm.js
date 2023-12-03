39b0bce63f213b0e69f9205638c8333ac3c9358e
Geeth <induwarakwg3573@gmail.com>
2023-12-03 22:55:00 +0530

    Add form submission functionality to TodoForm component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
         placeholder="What is the task today?"
+        onChange={(e) => setValue(e.target.value)}
       />
       <button type="submit" className="todo-btn">
-        Add
+        Add Task
       </button>
     </form>
   );

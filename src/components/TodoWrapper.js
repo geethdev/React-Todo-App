f13471473c119ca3c2c7e873f4c741ec2859e1f0
Geeth <induwarakwg3573@gmail.com>
2023-12-06 17:13:08 +0530

    Add toggleComplete functionality to Todo component

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -14,11 +14,21 @@ const TodoWrapper = () => {
     ]);
     console.log(todos);
   };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
     <div className="TodoWrapper">
+      <h1>Getting Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
       ))}
     </div>
   );
